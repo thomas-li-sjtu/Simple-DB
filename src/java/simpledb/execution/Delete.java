e51849859a698df9a0b255cf58c357fc3a143480
thomas-li-sjtu <980226547@qq.com>
2022-06-16 17:00:14 +0800

    finish lab2 exercise4

--- src/java/simpledb/execution/Delete.java
+++ src/java/simpledb/execution/Delete.java
@@ -11,6 +11,7 @@ import simpledb.transaction.TransactionAbortedException;
 import simpledb.transaction.TransactionId;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 /**
  * The delete operator. Delete reads tuples from its child operator and removes
@@ -20,59 +21,92 @@ public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private final TransactionId transactionId;
+
+    private OpIterator childOp;
+
+    private int count;
+
+    private boolean hasDeleted;
+
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
-     * 
-     * @param t
-     *            The transaction this delete runs in
-     * @param child
-     *            The child operator from which to read tuples for deletion
+     *
+     * @param t     The transaction this delete runs in
+     * @param child The child operator from which to read tuples for deletion
      */
     public Delete(TransactionId t, OpIterator child) {
         // some code goes here
+        this.transactionId = t;
+        this.childOp = child;
+        this.count = 0;
+        this.hasDeleted = false;  // 防止重复操作
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return new TupleDesc(new Type[]{Type.INT_TYPE});
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        super.open();
+        this.childOp.open();
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        this.childOp.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        this.childOp.rewind();
     }
 
     /**
      * Deletes tuples as they are read from the child operator. Deletes are
      * processed via the buffer pool (which can be accessed via the
      * Database.getBufferPool() method.
-     * 
+     *
      * @return A 1-field tuple containing the number of deleted records.
      * @see Database#getBufferPool
      * @see BufferPool#deleteTuple
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
+        if (!this.hasDeleted) {
+            while (this.childOp.hasNext()) {
+                Tuple tupleDelete = this.childOp.next();
+                try {
+                    Database.getBufferPool().deleteTuple(this.transactionId, tupleDelete);
+                    this.count += 1;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Tuple tuple = new Tuple(new TupleDesc(new Type[]{Type.INT_TYPE}));
+            tuple.setField(0, new IntField(this.count));
+            this.hasDeleted = true;
+            return tuple;
+        }
         return null;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[]{this.childOp};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        if (children != null && children.length > 0) {
+            this.childOp = children[0];
+        }
     }
 
 }
--- src/java/simpledb/execution/Insert.java
+++ src/java/simpledb/execution/Insert.java
@@ -2,11 +2,15 @@ package simpledb.execution;
 
 import simpledb.common.Database;
 import simpledb.common.DbException;
+import simpledb.common.Type;
 import simpledb.storage.BufferPool;
+import simpledb.storage.IntField;
 import simpledb.storage.Tuple;
 import simpledb.storage.TupleDesc;
 import simpledb.transaction.TransactionAbortedException;
 import simpledb.transaction.TransactionId;
+
+import java.io.IOException;
 
 /**
  * Inserts tuples read from the child operator into the tableId specified in the
@@ -16,39 +20,55 @@ public class Insert extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private final TransactionId transactionId;
+
+    private OpIterator childOp;
+
+    private final int tableId;
+
+    private int count;
+
+    private boolean hasInserted;
+
     /**
      * Constructor.
      *
-     * @param t
-     *            The transaction running the insert.
-     * @param child
-     *            The child operator from which to read tuples to be inserted.
-     * @param tableId
-     *            The table in which to insert tuples.
-     * @throws DbException
-     *             if TupleDesc of child differs from table into which we are to
-     *             insert.
+     * @param t       The transaction running the insert.
+     * @param child   The child operator from which to read tuples to be inserted.
+     * @param tableId The table in which to insert tuples.
+     * @throws DbException if TupleDesc of child differs from table into which we are to
+     *                     insert.
      */
     public Insert(TransactionId t, OpIterator child, int tableId)
             throws DbException {
         // some code goes here
+        this.transactionId = t;
+        this.childOp = child;
+        this.tableId = tableId;
+        this.count = 0;
+        this.hasInserted = false;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return new TupleDesc(new Type[]{Type.INT_TYPE});
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        super.open();
+        childOp.open();
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        childOp.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        childOp.rewind();
     }
 
     /**
@@ -60,23 +80,41 @@ public class Insert extends Operator {
      * duplicate before inserting it.
      *
      * @return A 1-field tuple containing the number of inserted records, or
-     *         null if called more than once.
+     * null if called more than once.
      * @see Database#getBufferPool
      * @see BufferPool#insertTuple
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
+        if (!this.hasInserted) {
+            while (this.childOp.hasNext()) {
+                Tuple tupleDelete = this.childOp.next();
+                try {
+                    Database.getBufferPool().insertTuple(this.transactionId, this.tableId, tupleDelete);
+                    this.count += 1;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Tuple tuple = new Tuple(new TupleDesc(new Type[]{Type.INT_TYPE}));
+            tuple.setField(0, new IntField(this.count));
+            this.hasInserted = true;
+            return tuple;
+        }
         return null;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[]{this.childOp};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        if (children != null && children.length > 0) {
+            this.childOp = children[0];
+        }
     }
 }
